68d2d888fe26c5e9327d4344f53c664509c4f5c1
Brad Fischl <bradley.fischl@gmail.com>
2017-05-10 22:47:33 -0400

    adding Server code

---
+++ Serverside/Server.py
@@ -0,0 +1,19 @@
+import xmpp
+import sys
+import logging
+
+
+___author___ = "Brad Fischl"
+___license___ = "GPL"
+___version___ = "0.0.1"
+___maintainer___ = "Brad Fischl"
+___email___ = "bradley.fischl {at} gmail"
+___status___ = "Development"
+loggingLevel = logging.DEBUG
+
+
+def main(args):
+    print args
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
